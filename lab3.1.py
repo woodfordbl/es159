7166f666d6f8b456c40b15e21329a0e2fcfdeaa1
woodfordbl <blakewoodford@college.harvard.edu>
2023-11-06 13:59:35 -0500

    update square

--- lab3.1.py
+++ lab3.1.py
@@ -1,16 +1,17 @@
 from toolkit import robo_toolkit as rt
-from toolkit import robo_commands as rc
+#from toolkit import robo_commands as rc
 from lab3.shapes import plot_square
 import numpy as np
 
 # Set np print options to display 4 decimal places
 np.set_printoptions(precision=3, suppress=True)
 
-points = plot_square(0, 500, 500, 100, 10)
+points = plot_square(0, 500, 500, 100, 10, show=True)
 
 # Now we want to create the joint angles that will move the arm to the elipse
 lab_robot = rt.ScrewLabRobot()
 
+""""
 guess = [0, 0, 0, 0, 0, 0]
 time = 10
 pose = np.eye(3)
@@ -21,5 +22,7 @@ armCmd, robotCmd, velCmd = rc.init_robot()
 time_step = 2
 
 for angle in angles:
+    # convert angle to list
     message = rc.create_position_message(angle, velocities, time=2)
     rc.publish_position_message(message=message, armCmd=armCmd, robotCmd=robotCmd)
+"""
--- lab3/shapes.py
+++ lab3/shapes.py
@@ -1,6 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+def create_color_gradient(num_points):
+    # Define the start and end colors (blue and red)
+    start_color = (0, 0, 1)  # Blue
+    end_color = (1, 0, 0)    # Red
+
+    # Create a linear gradient of colors
+    colors = [(
+        start_color[0] + (end_color[0] - start_color[0]) * i / (num_points - 1),
+        start_color[1] + (end_color[1] - start_color[1]) * i / (num_points - 1),
+        start_color[2] + (end_color[2] - start_color[2]) * i / (num_points - 1)
+    ) for i in range(num_points)]
+
+    return colors
+
 # Trace an square in the xz plane that is centered at some value
 def plot_square(center_x, center_y, center_z, side_length, num_points, show=False):
     half_side = side_length / 2
@@ -11,27 +25,34 @@ def plot_square(center_x, center_y, center_z, side_length, num_points, show=Fals
 
     edge_points = []
 
-    # Add points along the top and bottom edges
+    # Add points along the top edge
     for x in x_points:
-        edge_points.append([x, center_y, center_z - half_side])
         edge_points.append([x, center_y, center_z + half_side])
 
-    # Add points along the left and right edges (excluding corners to avoid duplicates)
+    # Add points along the right edge
     for z in z_points[1:-1]:
-        edge_points.append([center_x - half_side, center_y, z])
         edge_points.append([center_x + half_side, center_y, z])
+
+    # Add points along the bottom edge (in reverse order)
+    for x in x_points[::-1]:
+        edge_points.append([x, center_y, center_z - half_side])
+
+    # Add points along the left edge (in reverse order)
+    for z in z_points[-2:0:-1]:
+        edge_points.append([center_x - half_side, center_y, z])
 
     # Convert the edge points to a NumPy array
     edge_points = np.array(edge_points)
 
+    # create list of colors in gradient
+    colors = create_color_gradient(len(edge_points))
 
     if show:
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
-        ax.scatter(edge_points[:, 0], edge_points[:, 1], edge_points[:, 2], c='b', marker='o')
-        
-        # Change color of the points from red to blue as the points move along the path
+        ax.scatter(edge_points[:, 0], edge_points[:, 1], edge_points[:, 2], c=colors, marker='o')
 
+        # Change color of the points from red to blue as the points move along the path
 
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
