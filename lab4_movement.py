2714d7413dfc4f022f99d4bcbeba9a14c5b95892
woodfordbl <blakewoodford@college.harvard.edu>
2023-11-19 15:45:44 -0500

    updated start position

--- lab4_movement.py
+++ lab4_movement.py
@@ -22,21 +22,24 @@ guess = [-np.pi/18, -np.pi/3, np.pi/2, -2*np.pi/3, -np.pi/2, 4*np.pi/9]
 
 rest_position = robot.ikine(rest, theta=guess)
 
-start = np.array([[-1,0,0,665],
-                 [0,1,0,0],
-                 [0,0,-1,-80],
-                 [0,0,0,1]])
+x, y, z = path[0]
+
+start = np.array([[-1,0,0,x],
+                  [0,1,0,y],
+                  [0,0,-1,100],
+                  [0,0,0,1]])
 
 start_position = robot.ikine(start, theta=rest_position)
 
 thetas = [start_position]
+
 for coord in path:
     x, y, z = coord
 
-    pose = start = np.array([[-1,0,0,x],
-                            [0,1,0,y],
-                            [0,0,-1,z],
-                            [0,0,0,1]])
+    pose = np.array([[-1,0,0,x],
+                    [0,1,0,y],
+                    [0,0,-1,z],
+                    [0,0,0,1]])
     guess = thetas[-1]
 
     robot_position = robot.ikine(pose, theta=guess)    
@@ -47,8 +50,11 @@ end_position = robot.fkine(thetas[-1])
 end_position[2,3] = 100
 thetas.append(robot.ikine(end_position, theta=thetas[-1]))
 
+# Move the robot to the rest position
+rc.publish_position_message(rc.create_position_message(rest, [0,0,0,0,0,0], time=1), armCmd, robotCmd)
+
 # Move the robot to the start position
-rc.publish_position_message(rc.create_position_message(start_position, [0,0,0,0,0,0], time=1), armCmd, robotCmd)
+rc.publish_position_message(rc.create_position_message(start_position, [0,0,0,0,0,0], time=2), armCmd, robotCmd)
 
 # Loop through the path and move the robot to each position
 for i in range(len(thetas)):
